ecc84e4d0596635569a8fa23e58e325c3a78b884
Dennis Dobirr <me@dennisdobirr.de>
2022-11-19 07:30:45 +0100

    finished assignment 2

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import {NavigationContainer} from "@react-navigation/native";
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
 
+
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs();
+
 export default function App() {
     return (
         <Provider store={store}>
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://192.168.1.94:3001/';
+export const baseUrl = 'http://192.168.178.26:3001/';
